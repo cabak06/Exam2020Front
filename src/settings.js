e209adc6b938f66b9f9107456908af42fd047edb
Cahit <cahit10@hotmail.com>
2020-06-16 03:01:54 +0200

    added small modifications to backendURL's

--- src/settings.js
+++ src/settings.js
@@ -2,6 +2,28 @@
 function URLS(){
 
     function backendURL() {
+        const URL = "https://cahits.dk/Exam2020";
+        return URL;
+    }
+
+    function externalApi() {
+        const URL = "https://cahits.dk/Exam2020/api/info/external";
+        return URL;
+    }
+
+    function movieInfoApi(title) {
+        const URL = "https://cahits.dk/Exam2020/api/movie-info/" + title;
+        return URL;
+    }
+
+    function movieInfoApiAll(title) {
+        const URL = "https://cahits.dk/Exam2020/api/movie-info-all-ratings/" + title;
+        return URL;
+    }
+
+
+/*
+    function backendURL() {
         const URL = "http://localhost:8080/startcode";
         return URL;
     }
@@ -20,7 +42,7 @@ function URLS(){
         const URL = "http://localhost:8080/startcode/api/movie-info-all-ratings/" + title;
         return URL;
     }
-
+*/
     return {
         backendURL,
         externalApi,
